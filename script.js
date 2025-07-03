9a19229d8f0d6533a8f4d6709a477b09477fcd63
KatePASSY <katemsf98@gmail.com>
2025-07-03 10:11:58 +0200

    V1

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', chargerMenu);
+
+async function chargerMenu() {
+  try {
+    const header = document.getElementById('header');
+    const reponse = await fetch('html/header.html');
+
+    if (!reponse.ok) {
+      throw new Error('Erreur lors du chargement du menu');
+    }
+
+    const html = await reponse.text();
+    header.innerHTML = html;
+
+  } catch (erreur) {
+    console.error('Erreur :', erreur);
+  }
+
+let menuBtn = document.getElementById("menu-btn");
+let menu = document.getElementById("menu-list");
+menuBtn.addEventListener("click", function(){
+   menu.classList.toggle("active");
+  });
+
+}
+
+
